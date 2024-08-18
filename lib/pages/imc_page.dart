3c2cd5374f82eb8da7f9592262b2839dddd54a99
androsgithub <andreemaprinc@gmail.com>
2024-08-18 18:44:45 -0300

    create pages folder

---
+++ lib/pages/imc_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IMCPage extends StatelessWidget {
+  const IMCPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
