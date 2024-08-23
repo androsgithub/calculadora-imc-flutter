92721580883246a39e78e642bb9a4926ccd20fb2
androsgithub <andreemaprinc@gmail.com>
2024-08-23 20:51:49 -0300

    refactor: add hive and path configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:calculadora_imc_flutter/classes/pessoa_model.dart';
 import 'package:calculadora_imc_flutter/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async {
+  var documentPath = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(documentPath.path);
+  Hive.registerAdapter(PessoaModelAdapter());
+
   runApp(const MyApp());
 }
